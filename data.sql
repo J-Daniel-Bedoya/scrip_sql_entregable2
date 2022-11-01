55b45cfe1064c03f677b5dee7b291e9a0134da39
daniel1718 <jbedoyachavarriaga@gmail.com>
2022-10-31 20:59:08 -0500

    firs commit

--- data.sql
+++ data.sql
@@ -1,44 +1,44 @@
+CREATE TABLE "posts" (
+  "id_post" SERIAL PRIMARY KEY,
+  "title" varchar,
+  "description" varchar,
+  "author" varchar,
+  "text" varchar,
+  "id_tags" int NOT NULL,
+  "id_user_comment" int NOT NULL
+);
+
 CREATE TABLE "user" (
-  "id" SERIAL PRIMARY KEY,
+  "id_user" SERIAL PRIMARY KEY,
   "name" varchar,
   "email" varchar NOT NULL,
   "password" varchar NOT NULL,
   "age" int
 );
 
-CREATE TABLE "posts" (
-  "id" SERIAL PRIMARY KEY,
-  "title" varchar,
-  "description" varchar,
-  "author" varchar,
-  "text" varchar,
-  "id_tgs" int,
-  "id_ref" int
-);
-
 CREATE TABLE "tags" (
-  "id" SERIAL PRIMARY KEY,
+  "id_tags" SERIAL PRIMARY KEY,
   "name" varchar
 );
 
 CREATE TABLE "comments" (
-  "id" SERIAL PRIMARY KEY,
+  "id_comment" SERIAL PRIMARY KEY,
   "comment" varchar
 );
 
 CREATE TABLE "rel_user_comment" (
-  "id" SERIAL PRIMARY KEY,
-  "id_user" int,
-  "id_comment" int
+  "id_user_comment" SERIAL PRIMARY KEY,
+  "id_user" int NOT NULL,
+  "id_comment" int NOT NULL
 );
 
-ALTER TABLE "rel_user_comment" ADD FOREIGN KEY ("id_user") REFERENCES "user" ("id");
+ALTER TABLE "posts" ADD FOREIGN KEY ("id_tags") REFERENCES "tags" ("id_tags");
 
-ALTER TABLE "rel_user_comment" ADD FOREIGN KEY ("id_comment") REFERENCES "comments" ("id");
+ALTER TABLE "posts" ADD FOREIGN KEY ("id_user_comment") REFERENCES "rel_user_comment" ("id_user_comment");
 
-ALTER TABLE "posts" ADD FOREIGN KEY ("id_tgs") REFERENCES "tags" ("id");
+ALTER TABLE "rel_user_comment" ADD FOREIGN KEY ("id_user") REFERENCES "user" ("id_user");
 
-ALTER TABLE "posts" ADD FOREIGN KEY ("id_ref") REFERENCES "rel_user_comment" ("id");
+ALTER TABLE "rel_user_comment" ADD FOREIGN KEY ("id_comment") REFERENCES "comments" ("id_comment");
 
 insert into "user" (name, email, password, age) values ('Diana', 'diana@gmail.com', 123, 19),
 ('Daniel', 'daniel@gmail.com', 321, 18);
@@ -49,6 +49,6 @@ insert into "comments" (comment) values ('¡Que bonito!'), ('¡Que buen pots!'),
 
 insert into "rel_user_comment" (id_user, id_comment) values (2,1), (1,2), (1,3), (2,4);
 
-insert into "posts" (title, description, author, text, id_tgs, id_ref) values 
-('datos curiosos', 'frases mas conocidas del mundo medieval','Sócrates', 'lorem', 1, 6), 
-('peluditos', '7 tips para que tu mascota se comporte mejor','Estrellita777', 'lorem',2,7);
+insert into "posts" (title, description, author, text, id_tags, id_user_comment) values 
+('datos curiosos', 'frases mas conocidas del mundo medieval','Sócrates', 'lorem', 1, 2), 
+('peluditos', '7 tips para que tu mascota se comporte mejor','Estrellita777', 'lorem',2,1);
